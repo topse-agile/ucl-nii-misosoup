09fa51a1fe9243c17b7da7090fadb8e137b2ecc9
hirura <hirura@gmail.com>
2018-02-14 18:12:50 +0900

    update DataStore test with minimal requirement

--- src/test/java/jp/topse/misosoup/DataStoreTest.java
+++ src/test/java/jp/topse/misosoup/DataStoreTest.java
@@ -1,6 +1,24 @@
 package jp.topse.misosoup;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 import org.junit.Test;
 
 public class DataStoreTest {
+    @Test
+    public void testGetApplePriceReturns100()
+    {
+        DataStore dataStore = new DataStore();
+
+        assertThat(stockManager.getPrice("Apple"), is(100));
+    }
+
+    @Test
+    public void testGetOrangePriceReturns100()
+    {
+        DataStore dataStore = new DataStore();
+
+        assertThat(stockManager.getPrice("Orange"), is(200));
+    }
 }
