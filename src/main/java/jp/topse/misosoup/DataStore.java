391c79d0f0e9b693bf0a1ba9e9e71059d279a753
CB\tc08 <yoshihara@level-five.jp>
2018-02-15 13:31:57 +0900

    DataStore added

---
+++ src/main/java/jp/topse/misosoup/DataStore.java
@@ -0,0 +1,21 @@
+package jp.topse.misosoup;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataStore {
+   private Map<String, Integer> items = new HashMap<String, Integer>();
+   
+   public DataStore() {
+	   items.put("Apple",100);
+	   items.put("Orange",200);
+   }
+   
+   public int getPrice(String item){
+	   Integer price = items.get(item);
+	   return price;
+   }
+   public void updateItem(String item, Integer price){
+	   items.put(item, price);
+   }
+}
